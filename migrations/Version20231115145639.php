b82e940a7e1b0d5c9b37df61dd4a57ba6642b0ea
UgoRastell <urastell@normandiewebschool.fr>
2023-11-15 16:43:28 +0100

    first commit release

---
+++ migrations/Version20231115145639.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231115145639 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE material (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, quantity INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE reservation (id INT AUTO_INCREMENT NOT NULL, material_id INT NOT NULL, emprunt_date DATETIME NOT NULL, rendered DATETIME NOT NULL, is_rendered TINYINT(1) NOT NULL, student_id INT NOT NULL, INDEX IDX_42C84955E308AC6F (material_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE reservation ADD CONSTRAINT FK_42C84955E308AC6F FOREIGN KEY (material_id) REFERENCES material (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE reservation DROP FOREIGN KEY FK_42C84955E308AC6F');
+        $this->addSql('DROP TABLE material');
+        $this->addSql('DROP TABLE reservation');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
---
+++ tests/Controller/MaterialControllerTest.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Test\Controller;
+
+use App\Entity\Material;
+use App\Repository\MaterialRepository;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class MaterialControllerTest extends WebTestCase
+{
+    private KernelBrowser $client;
+    private MaterialRepository $repository;
+    private string $path = '/material/';
+
+
+  
+
+     protected function setUp(): void
+    {
+        $this->client = static::createClient();
+        $this->repository = static::getContainer()->get('doctrine')->getRepository(Material::class);
+
+        //remettre le test a zero a chaque fois 
+        foreach ($this->repository->findAll() as $object) {
+            $this->repository->remove($object, true);
+        }
+    }
+
+    public function testIndex(): void
+    {
+        $crawler = $this->client->request('GET', $this->path);
+
+        self::assertResponseStatusCodeSame(200);
+
+    }
+
+    
+    public function testNew(): void
+    {
+        $originalNumObjectsInRepository = count($this->repository->findAll());
+
+        $this->client->request('GET', sprintf('%snew', $this->path));
+
+        self::assertResponseStatusCodeSame(200);
+
+        $this->client->submitForm('Sauvegarder', [
+            'material[name]' => 'Testing',
+            'material[quantity]' => 16,
+        ]);
+
+
+       self::assertSame($originalNumObjectsInRepository + 1, count($this->repository->findAll()));
+    }
+
+
+    // public function testShow(): void
+    // {
+    //     $this->markTestIncomplete();
+    //     $fixture = new Material();
+    //     $fixture->setName('My Title');
+    //     $fixture->setQuantity('My Title');
+
+    //     $this->repository->add($fixture, true);
+
+    //     $this->client->request('GET', sprintf('%s%s', $this->path, $fixture->getId()));
+
+    //     self::assertResponseStatusCodeSame(200);
+    //     self::assertPageTitleContains('Material');
+
+    //     // Use assertions to check that the properties are properly displayed.
+    // }
+
+
+    
+    public function testEdit(): void
+    {
+        $fixture = new Material();
+        $fixture->setName('Title');
+        $fixture->setQuantity(800);
+
+        $this->repository->save($fixture, true);
+
+        $this->client->request('GET', sprintf('%s%s/edit', $this->path, $fixture->getId()));
+
+        $this->client->submitForm('Envoyer', [
+            'material[name]' => 'TitleTest',
+            'material[quantity]' => 878,
+        ]);
+
+        // self::assertResponseRedirects('/material/');
+
+        // find all = tableau de tout les objets de la table material
+        $fixture = $this->repository->findAll();
+
+        self::assertSame('TitleTest', $fixture[0]->getName());
+        self::assertSame(878, $fixture[0]->getQuantity());
+    }
+  
+    
+
+
+    public function testRemove(): void
+    {
+
+        $originalNumObjectsInRepository = count($this->repository->findAll());
+
+        $fixture = new Material();
+        $fixture->setName('TitleTest');
+        $fixture->setQuantity(800);
+
+        $this->repository->save($fixture, true);
+
+        self::assertSame($originalNumObjectsInRepository + 1, count($this->repository->findAll()));
+
+        $this->client->request('GET', sprintf('%s%s', $this->path, $fixture->getId()));
+        $this->client->submitForm('Supprimer');
+
+        self::assertSame($originalNumObjectsInRepository, count($this->repository->findAll()));
+    }
+}
